636bf19c4978a1c8fa1997c26318cdc4ea591f30
alecoexposito <alecoexposito@gmail.com>
2018-04-09 10:33:35 -0400

    modifying method that returns user's devices

--- components/devices/entities/Device.js
+++ components/devices/entities/Device.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes)=>{
         remark: { type: DataTypes.STRING},
         activation_date: { type: DataTypes.DATE},
         expiration_date: { type: DataTypes.DATE},
-        devices_group_id: { type: DataTypes.STRING},
+        devices_group_id: { type: DataTypes.INTEGER},
     }, {freezeTableName: false});
     return device;
 };
