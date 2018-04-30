8f21f27c96759fbcc8ac5f2a637f0d9b78d17931
alecoexposito <alecoexposito@gmail.com>
2018-04-30 15:59:55 -0400

    loggin with username instead of email

--- components/alarm/middleware.js
+++ components/alarm/middleware.js
@@ -4,6 +4,7 @@ var middleware = {
     storeAlarms: function(req, res) {
         try {
             let data = factory._storeAlarms(req.body);
+            console.log("llego a la api aki", req.body);
             res.status(200);
             res.json(data);
         } catch (error) {
