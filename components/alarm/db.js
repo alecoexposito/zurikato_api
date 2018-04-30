8abb215d40c0b7d53a75dda8e92debbc70ad0bd0
alecoexposito <alecoexposito@gmail.com>
2018-04-30 16:57:48 -0400

    loggin with username instead of email

--- components/alarm/db.js
+++ components/alarm/db.js
@@ -4,6 +4,8 @@ var sequelize = require('../../lib/db/dbEngines/mysql/mysql');
 const db = {};
 var alarm = sequelize.import(path.join(__dirname,'entities/alarm'));
 var alarm_code = sequelize.import(path.join(__dirname,'entities/alarmCode'));
+db.devices = require('../devices/entities/Device')(sequelize, Sequelize);
+
 db[alarm.name] = alarm;
 db[alarm_code.name] = alarm_code;
 db[alarm.name].associate(db);
