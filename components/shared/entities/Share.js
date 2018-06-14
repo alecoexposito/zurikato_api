3b6e047acc711053e0a8e0e50c39d27db17d38e6
alecoexposito <alecoexposito@gmail.com>
2018-06-14 15:32:45 -0400

    adding tables for share functionality

--- components/shared/entities/Share.js
+++ components/shared/entities/Share.js
@@ -7,6 +7,8 @@ module.exports = (sequelize, DataTypes) => {
         },
         expiration_date: { type: DataTypes.DATE },
         url_hash: { type: DataTypes.STRING },
+        created_at: { type: DataTypes.DATE },
+        updated_at: { type: DataTypes.DATE },
     }, { freezeTableName: false });
     return shares;
 };
