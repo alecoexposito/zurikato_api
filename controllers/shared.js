300f350ce0835dd84b280e8561af432ccef7d94f
alecoexposito <alecoexposito@gmail.com>
2018-06-14 15:08:44 -0400

    adding tables for share functionality

--- controllers/shared.js
+++ controllers/shared.js
@@ -1,10 +1,10 @@
-var sharedMiddleware = require(__dirname + '/components/shared/repository');
+var sharedMiddleware = require(__dirname + '../components/shared/repository');
 var shared = {
 
     getSharedScreen: async function(req, res) {
         res.json(sharedMiddleware.getSharedScreen(req.params.id));
     }
-    
+
 };
 
 module.exports = shared;
