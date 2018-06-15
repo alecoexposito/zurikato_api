2bf166984286632a461343cca0705c43e61d30df
alecoexposito <alecoexposito@gmail.com>
2018-06-15 13:55:01 -0400

    adding save share functionality

--- components/shared/repository.js
+++ components/shared/repository.js
@@ -29,7 +29,7 @@ const repository = {
         let share = {
             expiration_date: expirationDate,
             url_hash: '2134314314',
-            devices: [device]
+            devices: [{ idDevice: device.idDevice }]
         };
         let s = await db.shares.create(share, {
             include: [{
