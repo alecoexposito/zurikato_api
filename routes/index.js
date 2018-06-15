6426568d85399d576249d1a58c6bc03edefa7148
alecoexposito <alecoexposito@gmail.com>
2018-06-15 11:35:28 -0400

    adding save share functionality

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/profile/:id', auth.profile);
 
 router.get('/gov/vehicles-only-gps', devices.getDevicesLastData);
 router.get('/shared-screen/:id', shared.getSharedScreen);
-router.get('/save-shared', shared.saveShared);
+router.post('/save-shared', shared.saveShared);
 
 
 router.get('/devices', devices.getAll);
