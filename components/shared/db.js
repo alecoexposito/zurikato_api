f0f2950aca54bf63abf54a9228914b3b45e11cb0
alecoexposito <alecoexposito@gmail.com>
2018-06-19 14:55:18 -0400

    adding save share functionality

--- components/shared/db.js
+++ components/shared/db.js
@@ -7,7 +7,7 @@ db.sequelize = sequelize;
 db.devices = require('../../devices/entities/Device')(sequelize, Sequelize);
 db.shares = require('./entities/Share')(sequelize, Sequelize);
 db.deviceShare = require('./entities/DeviceShare')(sequelize, Sequelize);
-db.devices.belongsToMany(db.shares, {through: 'device_share', foreignKey: 'device_id'});
-db.shares.hasMany(db.devices, {through: 'device_share', foreignKey: 'share_id', as: 'devices'});
+db.devices.belongsToMany(db.shares, {through: 'device_shares', foreignKey: 'device_id'});
+db.shares.hasMany(db.devices, {through: 'device_shares', foreignKey: 'share_id', as: 'devices'});
 
 module.exports = db;
--- components/shared/entities/DeviceShare.js
+++ components/shared/entities/DeviceShare.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const deviceShare = sequelize.define('device_share', {
+    const deviceShare = sequelize.define('device_shares', {
         device_id: { type: DataTypes.INTEGER },
         share_id: { type: DataTypes.INTEGER },
     }, { freezeTableName: false });
