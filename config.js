1dc698574b91d4f214ad7bfe284666e935d7e901
alecoexposito <alecoexposito@gmail.com>
2018-06-19 17:10:36 -0400

    adding save share functionality

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    db: { 'host': 'localhost', 'user': 'root', 'passwd': 'BlBxsRv2018', 'name': 'zurikato_api' },
+    db: { 'host': 'localhost', 'user': 'root', 'passwd': 'Shenzhen2018', 'name': 'zurikato_api' },
     hash: { 'iterations': 10 }
 };
 module.exports = config;
