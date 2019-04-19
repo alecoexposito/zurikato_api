c20f893661d8e90fff8a8cfa5ea2691c23d23f8f
Alejandro <alecoexposito@gmail.com>
2019-04-18 22:47:26 -0400

    change for C5 test

--- Factories/deviceFactory.js
+++ Factories/deviceFactory.js
@@ -1,6 +1,9 @@
 var jwt = require('jsonwebtoken');
 var secret = require('../secret');
 var repository = require('../lib/db/repository');
+var config = require('../config');
+var http = require('http');
+
 var deviceFactory = {
 
     _getDevices: async function(token) {
@@ -67,14 +70,42 @@ var deviceFactory = {
 
     getDevicesLastData: async function() {
         try {
-            var data = await repository.devices.getDevicesLastData();
-            dataArray = [];
-            var result = { ArrayOfVehiclesOnlyGps_Result: dataArray};
-            data.forEach(function(value) {
-                dataArray.push({
-                    VehicleOnlyGps_Result: value
+
+            var options = {
+                hostname: config.mdvrApiIp,
+                port: config.mdvrApiPort,
+                path: '/StandardApiAction_login.action?account=' + config.mdvrApiUser + '&password=' + config.mdvrApiPass,
+                method: 'GET'
+            };
+            var jsession = "";
+            http.request(_this.options, function (res) {
+                console.log('LOGIN STATUS: ' + res.statusCode);
+                // console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    // console.log("first time data: ", data);
+                    jsession = JSON.parse(data).jsession;
+                    console.log("JSESSION: ", _this.jsession);
+                    if (jsession == "")
+                        return;
+                    var data = await repository.devices.getDevicesLastData();
+                    dataArray = [];
+                    var result = { ArrayOfVehiclesOnlyGps_Result: dataArray};
+                    data.forEach(function(value) {
+                        var param3 = "jsession" + ",3," + value.IMEI + ",0,1,0,0";
+                        var param3Base64 = btoa(param3);
+                        var cameraUrl = "rtsp://209.126.127.171:6604/" + param3Base64;
+                        value.UrlCamera = cameraUrl;
+                        dataArray.push({
+                            VehicleOnlyGps_Result: value
+                        });
+                    });
                 });
-            });
+            }).end();
+
+
+
             return result;
         }catch(error){
             return false;
