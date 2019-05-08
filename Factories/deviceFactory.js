5b75cb234f70b47417104e1b48a8ee166bb0e839
Alejandro <alecoexposito@gmail.com>
2019-05-08 10:46:12 -0400

    change for C5 test

--- Factories/deviceFactory.js
+++ Factories/deviceFactory.js
@@ -99,7 +99,7 @@ var deviceFactory = {
             data.forEach(function(value) {
                 if(jsession != undefined) {
                     var dateStr = value.Date + " " + value.Time;
-                    var utcDate = moment(dateStr, 'DD-MM-YYYY HH:mm:ss');
+                    var utcDate = moment.utc(dateStr, 'DD-MM-YYYY HH:mm:ss');
                     var localDate = utcDate.local();
 
                     var param3 = jsession + ",3," + value.IMEI + ",0,1,0,0";
