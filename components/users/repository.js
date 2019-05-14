978186675cc587d94a375f5ead2d098f8e3f180e
Alejandro <alecoexposito@gmail.com>
2019-05-14 16:49:34 -0400

    change for C5 test

--- components/users/repository.js
+++ components/users/repository.js
@@ -42,29 +42,31 @@ const repository = {
     getDevices: async function(id) {
         console.log("---------------------- GET DEVICES -------------------------");
          try {
-            var devices = await db.devices.findAll({
-            where: {
-                $and: [
-                    { user_id: id },
-                ]
-            },
-            include: [{
-                model: db.deviceModel
-            }, {
-                model: db.gpsData,order: [['createdAt','DESC']],limit:1
-            }, {
-                model: db.vehicle
-            }, {
-                model: db.users
-            }]
-            
-            });
+            // var devices = await db.devices.findAll({
+            // where: {
+            //     $and: [
+            //         { user_id: id },
+            //     ]
+            // },
+            // include: [{
+            //     model: db.deviceModel
+            // }, {
+            //     model: db.gpsData,order: [['createdAt','DESC']],limit:1
+            // }, {
+            //     model: db.vehicle
+            // }, {
+            //     model: db.users
+            // }]
+            //
+            // });
+
+             var devices = db.sequelize.query("SELECT `devices`.`idDevice`, `devices`.`auth_device`, `devices`.`auth_password`, `devices`.`idDeviceModel`, `devices`.`label`, `devices`.`sim`, `devices`.`autoSync`, `devices`.`license_plate`, `devices`.`contact`, `devices`.`remark`, `devices`.`activation_date`, `devices`.`expiration_date`, `devices`.`panic_button`, `devices`.`trashed`, `devices`.`mdvr_number`, `devices`.`createdAt`, `devices`.`updatedAt`, `device_model`.`idDeviceModel` AS `device_model.idDeviceModel`, `device_model`.`label` AS `device_model.label`, `device_model`.`peripheral_gps` AS `device_model.peripheral_gps`, `device_model`.`peripheral_ticketsseller` AS `device_model.peripheral_ticketsseller`, `device_model`.`peripheral_cam1` AS `device_model.peripheral_cam1`, `device_model`.`peripheral_cam2` AS `device_model.peripheral_cam2`, `device_model`.`peripheral_cam3` AS `device_model.peripheral_cam3`, `device_model`.`peripheral_cam4` AS `device_model.peripheral_cam4`, `device_model`.`createdAt` AS `device_model.createdAt`, `device_model`.`updatedAt` AS `device_model.updatedAt`, `vehicle`.`id` AS `vehicle.id`, `vehicle`.`device_id` AS `vehicle.device_id`, `vehicle`.`client_id` AS `vehicle.client_id`, `vehicle`.`name` AS `vehicle.name`, `vehicle`.`plate_number` AS `vehicle.plate_number`, `vehicle`.`brand` AS `vehicle.brand`, `vehicle`.`model` AS `vehicle.model`, `vehicle`.`type` AS `vehicle.type`, `vehicle`.`year` AS `vehicle.year`, `vehicle`.`route` AS `vehicle.route`, `vehicle`.`odometer` AS `vehicle.odometer`, `vehicle`.`createdAt` AS `vehicle.createdAt`, `vehicle`.`updatedAt` AS `vehicle.updatedAt`, `users`.`idUser` AS `users.idUser`, `users`.`email` AS `users.email`, `users`.`label` AS `users.label`, `users`.`telephone` AS `users.telephone`, `users`.`pass` AS `users.pass`, `users`.`salt` AS `users.salt`, `users`.`userType` AS `users.userType`, `users`.`parent` AS `users.parent`, `users`.`active` AS `users.active`, `users`.`auth_token` AS `users.auth_token`, `users`.`token` AS `users.token`, `users`.`username` AS `users.username`, `users`.`automatic_imeis` AS `users.automatic_imeis`, `users`.`company_name` AS `users.company_name`, `users`.`fences` AS `users.fences`, `users`.`createdAt` AS `users.createdAt`, `users`.`updatedAt` AS `users.updatedAt`, `users->user_devices`.`idUserDevice` AS `users.user_devices.idUserDevice`, `users->user_devices`.`idUser` AS `users.user_devices.idUser`, `users->user_devices`.`idDevice` AS `users.user_devices.idDevice`, `users->user_devices`.`label` AS `users.user_devices.label`, `users->user_devices`.`createdAt` AS `users.user_devices.createdAt`, `users->user_devices`.`updatedAt` AS `users.user_devices.updatedAt` FROM `devices` AS `devices` LEFT OUTER JOIN `device_models` AS `device_model` ON `devices`.`idDeviceModel` = `device_model`.`idDeviceModel` LEFT OUTER JOIN `vehicle` AS `vehicle` ON `devices`.`idDevice` = `vehicle`.`device_id` LEFT OUTER JOIN ( `user_devices` AS `users->user_devices` INNER JOIN `users` AS `users` ON `users`.`idUser` = `users->user_devices`.`idUser`) ON `devices`.`idDevice` = `users->user_devices`.`idDevice` WHERE (`devices`.`user_id` = " + id + ")");
             console.log("------------- ANTES DE PEDIR LOS DISPOSITIVOS -------------------------");
             var result = devices;
             console.log("-------------- DESPUES DE PEDIR LOS DISPOSITIVOS ----------------------");
-            for(var i = 0; i < result.length; i++) {
-                result[i].company_name = result[i].users.company_name;
-            }
+            // for(var i = 0; i < result.length; i++) {
+            //     result[i].company_name = result[i].users.company_name;
+            // }
             console.log("result:  ", result);
             return result;
          }catch(error){console.log(error);}
