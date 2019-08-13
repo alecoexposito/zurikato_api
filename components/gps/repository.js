9d4078a417a84f52df01e9a16f4f90c2dc9a819c
Alejandro <alecoexposito@gmail.com>
2019-08-13 13:55:35 -0400

    change for C5 test

--- components/gps/repository.js
+++ components/gps/repository.js
@@ -23,6 +23,8 @@ const repository = {
         } else {
             data = await db.devices.findOne({ where: { auth_device: gpsData.device_id } });
         }
+        console.log("################ BUSCADO POR ESTE IMEI: ", gpsData.device_id);
+        console.log("################ RESULTADO DE DATA DESPUES DE BUSCAR ################################", data);
         if (data == null) {
             let device = { 'idDeviceModel': modelId, 'label': deviceLabel, 'auth_device': gpsData.device_id, 'mdvr_number': mdvrNumber };
             data = await db.devices.create(device);
