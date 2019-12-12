db6124e02b42a40fccf8b82e30072109ccbe8183
Alejandro <alecoexposito@gmail.com>
2019-12-11 23:52:46 -0500

    change for C5 test

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,7 +82,7 @@ var admin = {
     },
 
     logModem: async function (req, res) {
-        console.log(req.param('strength'));
+        console.log(req.query.strength);
         fs.appendFile("/var/log/modem-query.log", "Hey there!", function(err) {
             if(err) {
                 return console.log(err);
