9a2c2f34893d07b403d9ef7ab3c4cd892824179f
Alejandro <alecoexposito@gmail.com>
2019-12-11 23:34:38 -0500

    modem log

--- controllers/admin.js
+++ controllers/admin.js
@@ -82,7 +82,7 @@ var admin = {
     },
 
     logModem: async function (req, res) {
-        console.log(req.body);
+        console.log(req.params);
         fs.appendFile("/var/log/modem-query.log", "Hey there!", function(err) {
             if(err) {
                 return console.log(err);
