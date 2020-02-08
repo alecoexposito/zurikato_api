77cf0672e7dcee80688d510ddcf80cddb51c3cd1
Alejandro <alecoexposito@gmail.com>
2020-02-08 15:15:28 -0500

    change for historical offline data

--- components/devices/repository.js
+++ components/devices/repository.js
@@ -1,7 +1,7 @@
 var db = require('../../lib/db/db');
 const repository = {
     history: async function(id, startDate, endDate) {
-        let query = "SELECT date_format(`peripheral_gps_data`.`createdAt`,'%Y-%m-%d %H:%i:%s') as day,`peripheral_gps_data`.`lat`,`peripheral_gps_data`.`lng`, peripheral_gps_data.speed as speed, peripheral_gps_data.orientation_plain as orientation_plain FROM `peripheral_gps_data` AS `peripheral_gps_data` INNER JOIN `devices` AS `device` ON `peripheral_gps_data`.`idDevice` = `device`.`idDevice` WHERE str_to_date(`peripheral_gps_data`.`createdAt`, '%Y-%m-%d %H:%i:%s') BETWEEN str_to_date('" + startDate + "', '%Y-%m-%d %H:%i:%s') AND str_to_date('" + endDate + "', '%Y-%m-%d %H:%i:%s') AND `peripheral_gps_data`.`idDevice`=" + id + " order by str_to_date(`peripheral_gps_data`.`updatedAt`,'%Y-%m-%d %H:%i:%s') ASC;";
+        let query = "SELECT date_format(`peripheral_gps_data`.`createdAt`,'%Y-%m-%d %H:%i:%s') as day,`peripheral_gps_data`.`lat`,`peripheral_gps_data`.`lng`, peripheral_gps_data.speed as speed, peripheral_gps_data.orientation_plain as orientation_plain FROM `peripheral_gps_data` AS `peripheral_gps_data` INNER JOIN `devices` AS `device` ON `peripheral_gps_data`.`idDevice` = `device`.`idDevice` WHERE str_to_date(`peripheral_gps_data`.`createdAt`, '%Y-%m-%d %H:%i:%s') BETWEEN str_to_date('" + startDate + "', '%Y-%m-%d %H:%i:%s') AND str_to_date('" + endDate + "', '%Y-%m-%d %H:%i:%s') AND `peripheral_gps_data`.`idDevice`=" + id + " order by str_to_date(`peripheral_gps_data`.`createdAt`,'%Y-%m-%d %H:%i:%s') ASC;";
         let data = await db.sequelize.query(query);
         return data[0];
     },
