b70503d2b4f7ba5a09fb154293c892aa5637e3cf
Alejandro <alecoexposito@gmail.com>
2020-11-19 23:42:09 -0500

    fix vpn

--- worker.js
+++ worker.js
@@ -77,7 +77,7 @@ class Worker extends SCWorker {
         (async () => {
             for await (let status of socket.listener('connect')) {
                 console.log("conectado al server websocket del tracker");
-                var vpnChannel = socket.subscribe('vpn_' + id + '_channel'
+                var vpnChannel = socket.subscribe('vpn_' + id + '_channel');
                 app.post('/api/v1/start-vpn/:id', (req, res) => {
                     let id = req.params.id;
                     console.log('starting vpn for bb with id: ', id);
